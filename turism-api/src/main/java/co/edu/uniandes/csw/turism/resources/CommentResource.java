ffee91da6b7773c9b0f52d6247cd7d55d46b6e47
da.prieto1 <da.prieto12uniandes.edu.co>
2016-08-27 12:32:51 -0500

    R-16 Crear recurso para Comments y modificar el archivo peristence.xml

---
+++ turism-api/src/main/java/co/edu/uniandes/csw/turism/resources/CommentResource.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.turism.resources;
+
+import co.edu.uniandes.csw.auth.provider.StatusCreated;
+import co.edu.uniandes.csw.turism.api.ICommentLogic;
+import co.edu.uniandes.csw.turism.dtos.detail.CommentDetailDTO;
+import co.edu.uniandes.csw.turism.dtos.detail.TripDetailDTO;
+import co.edu.uniandes.csw.turism.entities.CommentEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author da.prieto1
+ */
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CommentResource {
+
+    @Inject
+    private ICommentLogic commentLogic;
+
+    @Context
+    private HttpServletResponse response;
+
+    @QueryParam("page")
+    private Integer page;
+
+    @QueryParam("limit")
+    private Integer maxRecords;
+
+    @PathParam("tipId")
+    private Long tripId;
+
+    @PathParam("clientId")
+    private Long clientId;
+
+    @POST
+    @StatusCreated
+    public CommentDetailDTO createTrip(CommentDetailDTO dto) {
+        return new CommentDetailDTO(commentLogic.createComment(clientId, tripId, dto.toEntity()));
+    }
+
+    private List<CommentDetailDTO> listEntity2DTO(List<CommentEntity> entityList) {
+        List<CommentDetailDTO> list = new ArrayList<>();
+        for (CommentEntity entity : entityList) {
+            list.add(new CommentDetailDTO(entity));
+        }
+        return list;
+    }
+
+    @POST
+    @StatusCreated
+    public CommentDetailDTO createComment(CommentDetailDTO dto) {
+        return new CommentDetailDTO(commentLogic.createComment(clientId, tripId, dto.toEntity()));
+    }
+}
